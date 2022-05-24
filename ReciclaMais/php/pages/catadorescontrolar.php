1fcce35022b0509f33fe9defc99996b374e04124
Luiz Fernando <74717128+LuizReis-dev@users.noreply.github.com>
2022-05-24 19:41:27 -0300

    criando pagina de controlar catadores e  design de tabelas

---
+++ ReciclaMais/php/pages/catadorescontrolar.php
@@ -0,0 +1,46 @@
+<?php
+require 'navegacao.html';
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Controlar catadores</title>
+    <link rel="import" href="navegacao.html">
+    <link rel="stylesheet" href="../css/catadoresrcontrolar.css">
+    <link rel="stylesheet" href="../css/tabelacontrolar.css">
+</head>
+
+<body>
+    <div class="conteudo-principal">
+        <div class="opcoes">
+            <a id="selecionado" class="opcao" href="#">Catadores</a>
+            <a class="opcao" href="#">Empresas</a>
+            <a class="opcao" href="#">Materias</a>
+        </div>
+        <table class="tabela-controlar">
+            <thead>
+                <th>Id</th>
+                <th>Nome</th>
+                <th>Idade</th>
+                <th>CPF</th>
+                <th class="th-actions">Ações</th>
+            </thead>
+            <tr>
+                <td>5</td>
+                <td>Franciso Neves</td>
+                <td>50</td>
+                <td>000.000.000-00</td>
+                <td class="acoes"><a href="#"><i class="bi bi-eye olho"></i></a> <a href='#'><button btn-delete class='buttons-template btn-delete'>Deletar</button> </a></td>
+            </tr>
+        </table>
+
+    </div>
+
+</body>
+
+</html>
