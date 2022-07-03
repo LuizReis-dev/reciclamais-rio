792400d5168ceff792508ac895090a0547329e40
Luiz Fernando <74717128+LuizReis-dev@users.noreply.github.com>
2022-07-03 12:35:20 -0300

    Criando Classe MateriaisEmOperacaoComercial

---
+++ ReciclaMais/java/src/java/entidades/MateriaisEmOperacaoComercial.java
@@ -0,0 +1,52 @@
+package entidades;
+
+public class MateriaisEmOperacaoComercial {
+    
+    private int id;
+    private OperacaoComercial operacaoComercial;
+    private Material material;
+    private int quantidadeEmKg;
+
+    public MateriaisEmOperacaoComercial() {
+    }
+
+    public MateriaisEmOperacaoComercial(OperacaoComercial operacaoComercial, Material material, int quantidadeEmKg) {
+        this.operacaoComercial = operacaoComercial;
+        this.material = material;
+        this.quantidadeEmKg = quantidadeEmKg;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public OperacaoComercial getOperacaoComercial() {
+        return operacaoComercial;
+    }
+
+    public void setOperacaoComercial(OperacaoComercial operacaoComercial) {
+        this.operacaoComercial = operacaoComercial;
+    }
+
+    public Material getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(Material material) {
+        this.material = material;
+    }
+
+    public int getQuantidadeEmKg() {
+        return quantidadeEmKg;
+    }
+
+    public void setQuantidadeEmKg(int quantidadeEmKg) {
+        this.quantidadeEmKg = quantidadeEmKg;
+    }
+    
+    
+}
