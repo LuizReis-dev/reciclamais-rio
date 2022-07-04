7de90b5ad2514f4a21feb05eb0c5ca3aa61d20b2
Luiz Fernando <74717128+LuizReis-dev@users.noreply.github.com>
2022-07-04 07:59:54 -0300

    Adicionando metodo para listar material

---
+++ ReciclaMais/java/src/java/dao/MateriaisDao.java
@@ -0,0 +1,29 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+import entidades.Material;
+
+public class MateriaisDao {
+    public static List<Material> getMateriais() {
+        List<Material> list = new ArrayList<Material>();
+        try{
+        Connection con = ConnectionDao.getConnection();
+        PreparedStatement ps = (PreparedStatement) con.prepareStatement("SELECT * FROM catador");
+        ResultSet rs = ps.executeQuery();
+        while(rs.next()){
+            Material material = new Material();
+            material.setId(rs.getInt("id"));
+            material.setNome(rs.getString("nome"));
+            material.setPreco_por_kg(rs.getDouble("preco_por_kg"));
+            list.add(material);
+        }       
+    }catch(Exception erro){
+        System.out.println(erro);
+    }
+        return list;
+    }
+}
