195d14096a7832d01e57093645ab8df3a3241f26
Luiz Fernando <74717128+LuizReis-dev@users.noreply.github.com>
2022-07-04 08:08:56 -0300

    Criando api de listar materiais

---
+++ ReciclaMais/java/src/java/api/MateriaisApi.java
@@ -0,0 +1,36 @@
+package api;
+import dao.MateriaisDao;
+import entidades.Material;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+
+@WebServlet(name = "MateriaisApi", urlPatterns = {"/materiais"})
+public class MateriaisApi extends HttpServlet {
+     @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+         List<Material> listaMateriais = MateriaisDao.getMateriais();
+         
+         JSONArray retorno = new JSONArray(); 
+         
+         for(int i = 0; i <listaMateriais.size(); i++){
+             JSONObject objetoMateriais = new JSONObject();
+             objetoMateriais.put("id", listaMateriais.get(i).getId());
+             objetoMateriais.put("nome", listaMateriais.get(i).getNome());
+             objetoMateriais.put("preco_por_kg", listaMateriais.get(i).getPreco_por_kg());
+             retorno.put(objetoMateriais);
+         }
+         PrintWriter out = res.getWriter();
+         out.print(retorno.toString());
+         out.flush();
+    }
+    
+}
