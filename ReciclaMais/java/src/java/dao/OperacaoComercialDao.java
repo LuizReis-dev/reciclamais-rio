bbe2e3528fc4d4dc8ed92b992301f1ee4456602c
Luiz Fernando <74717128+LuizReis-dev@users.noreply.github.com>
2022-07-03 21:26:33 -0300

    Adicionando retorno do id da operacao comercial no metodo inserirCompra

--- ReciclaMais/java/src/java/dao/OperacaoComercialDao.java
+++ ReciclaMais/java/src/java/dao/OperacaoComercialDao.java
@@ -3,20 +3,27 @@ package dao;
 import entidades.OperacaoComercial;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class OperacaoComercialDao {
-    
-    public static int inserirCompra(OperacaoComercial operacaoComercial){
-       int status = 0;  
-   try{
-        Connection con = ConnectionDao.getConnection();
-        PreparedStatement ps = (PreparedStatement) con.prepareStatement("INSERT INTO operacao_comercial(id_catador,tipo,total) VALUES(?,'c',?)");
-        ps.setInt(1, operacaoComercial.getCatador().getId());
-        ps.setDouble(2, operacaoComercial.getTotal());
-        status = ps.executeUpdate();
-    }catch(Exception erro){
-        System.out.println(erro);
-    }      
-       return status;
-   }
+
+    public static int inserirCompra(OperacaoComercial operacaoComercial) {
+        int idOperacaoComercial = 0;
+        try {
+            Connection con = ConnectionDao.getConnection();
+            PreparedStatement ps = (PreparedStatement) con.prepareStatement("INSERT INTO operacao_comercial(id_catador,tipo,total) VALUES(?,'c',?)", Statement.RETURN_GENERATED_KEYS);
+            ps.setInt(1, operacaoComercial.getCatador().getId());
+            ps.setDouble(2, operacaoComercial.getTotal());
+            ps.executeUpdate();
+
+            ResultSet chaveGerada = ps.getGeneratedKeys();
+            if (chaveGerada.next()) {
+                idOperacaoComercial = chaveGerada.getInt(1);
+            }
+        } catch (Exception erro) {
+            System.out.println(erro);
+        }
+        return idOperacaoComercial;
+    }
 }
