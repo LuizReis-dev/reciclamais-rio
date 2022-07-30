280678d1e8c92ae6a458ebcaece77a14d9ba1920
Luiz Fernando <74717128+LuizReis-dev@users.noreply.github.com>
2022-07-30 10:38:45 -0300

    mostrando preço na tela conforme quantidade selecionada

--- ReciclaMais/site-ecommerce/carrinho.php
+++ ReciclaMais/site-ecommerce/carrinho.php
@@ -28,7 +28,7 @@ foreach ($materiais_no_carrinho as $material) {
                 <tr>
                     <td colspan="2">Produto</td>
                     <td>Preço</td>
-                    <td>Quantidade</td>
+                    <td>Quantidade em kg</td>
                     <td>Total</td>
                 </tr>
 
@@ -39,7 +39,7 @@ foreach ($materiais_no_carrinho as $material) {
                 $result = $conn->query($sql);
                 if ($result->num_rows > 0) {
                     while ($row = $result->fetch_assoc()) {
-                        echo '<tr>';
+                        echo '<tr class="material-adicionado" data-preco="'. $row["preco_venda_kg"].'" data-id="'.$row["id"].'">';
                         echo '<td class="img"><a href=""> <img src=">" width="50" height="50" alt=""></a> </td>';
                         echo ' <td>
                         <a href="">' . $row["nome"]. '</a>
@@ -48,9 +48,9 @@ foreach ($materiais_no_carrinho as $material) {
                         </td>
                         <td class="preco">R$'.$row["preco_venda_kg"].'</td>
                         <td class="quantidade">
-                        <input type="number" name="quantidade-'.$row["id"].'" value="" min="1" max="" placeholder="quantidade" required>
+                        <input class="quantidade-input" type="number" name="quantidade-'.$row["id"].'" value="1" min="1" max="" placeholder="quantidade em KG" required>
                         </td>
-                        <td class="preco">R$</td>';
+                        <td class="preco-calcular" id="preco-'.$row["id"].'">R$'.$row["preco_venda_kg"].'</td>';
                         echo '</tr>';
                     }
                 } else {
@@ -62,7 +62,7 @@ foreach ($materiais_no_carrinho as $material) {
         <p>Confira a quantidade selecionada antes de realizar a compra</p>
         <div class="subtotal">
             <span class="text">Preço total</span>
-            <span class="preco">$20,00</span>
+            <span id="subtotal" class="preco"></span>
         </div>
         <div class="buttons">
             <input type="submit" value="Atualizar" name="atualizar">
@@ -70,6 +70,29 @@ foreach ($materiais_no_carrinho as $material) {
         </div>
     </div>
 
+    <script>
+        let materiais = document.querySelectorAll('.material-adicionado');
+        let totalValor;
+        materiais.forEach((material, index) =>{
+            let totalDiv = document.getElementById(`preco-${material.dataset.id}`);
+            let quantidadeinput = document.getElementsByName(`quantidade-${material.dataset.id}`);
+            quantidadeinput[0].addEventListener('blur', ()=>{
+                totalValor = quantidadeinput[0].value * material.dataset.preco;
+                totalDiv.innerHTML = `R$${totalValor}`;
+                calcularValorFinal();
+            });
+        })
+        let calcularValorFinal = () => {
+            let valorFinal = 0;
+            let valores = document.querySelectorAll('.preco-calcular');
+            valores.forEach((valor) => {
+                valorFinal += parseFloat(valor.textContent.substr(2));
+            })
+            let preco = document.querySelector('#subtotal');
+            preco.innerHTML = `R$${valorFinal}`
+        }
+        calcularValorFinal();
+        </script>
 </body>
 
 </html>
