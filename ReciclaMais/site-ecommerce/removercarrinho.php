9e1e1968a121a9853ed284bc28e7ddb5ede2eb24
Luiz Fernando <74717128+LuizReis-dev@users.noreply.github.com>
2022-07-30 08:52:26 -0300

    adicionando função para remover itens do carrinho

---
+++ ReciclaMais/site-ecommerce/removercarrinho.php
@@ -0,0 +1,9 @@
+<?php 
+session_start();
+require 'conexao.php';
+//Checa se foi recebido o id do material, se o mesmo é um numero, se existe um carrinho e se o produto está no carrinho
+if(isset($_GET['id_material']) && is_numeric(($_GET['id_material'])) && isset($_SESSION['carrinho']) && isset($_SESSION['carrinho'][$_GET['id_material']])){
+    unset($_SESSION['carrinho'][$_GET['id_material']]);
+    header('location: carrinho.php');
+}
+?>
