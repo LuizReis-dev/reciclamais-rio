4d11c98fdc7c037c9cf164b4f403b2f72b0b0dcc
Luiz Fernando <74717128+LuizReis-dev@users.noreply.github.com>
2022-09-02 11:41:38 -0300

    reorganizando o código

--- ReciclaMais/site-ecommerce/principal/index.php
+++ ReciclaMais/site-ecommerce/principal/index.php
@@ -1,8 +1,5 @@
 <?php 
-require 'cabecalho.php';
-if(!isset($_SESSION["id_usuario"])){
-    header('location: login.html');
-}
+require '../util/cabecalho.php';
 
 ?>
 
@@ -14,16 +11,16 @@ if(!isset($_SESSION["id_usuario"])){
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Recicla Mais</title>
-    <link rel="stylesheet" href="./css/index.css">
+    <link rel="stylesheet" href="../css/index.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link rel="stylesheet" href="./css/estiloprincipal.css">
+    <link rel="stylesheet" href="../css/estiloprincipal.css">
 </head>
 
 <body>
    
     <div class="banner">
-        <img src="./imagens/cristo.png" alt="cristo-redentor">
+        <img src="../images/cristo.png" alt="cristo-redentor">
         <div class="paragrafo">
             <h2>Bem-vindo ao ReciclaMaisRio</h2>
             <p>A ReciclaMais Rio é uma cooperativa localizada nos subúrbios do Rio de Janeiro que compra matériais reciclaveis e vende para serem reutilizados, incentivando a reciclagem para um futuro melhor e verde desde 1998.</p>
--- ReciclaMais/site-ecommerce/util/cabecalho.php
+++ ReciclaMais/site-ecommerce/util/cabecalho.php
@@ -10,7 +10,7 @@ $qtd_carrinho = isset($_SESSION['carrinho']) ? count($_SESSION['carrinho']) : 0;
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="./css/cabecalho.css">
+    <link rel="stylesheet" href="../css/cabecalho.css">
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;800&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
 </head>
