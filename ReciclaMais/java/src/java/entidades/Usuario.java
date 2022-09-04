d32a22e7e4d69a4b6ea3b4678ae87a6c6e85142a
Luiz Fernando <74717128+LuizReis-dev@users.noreply.github.com>
2022-09-04 17:12:25 -0300

    Criando classe usuário

---
+++ ReciclaMais/java/src/java/entidades/Usuario.java
@@ -0,0 +1,60 @@
+package entidades;
+
+public class Usuario {
+    
+    private int id;
+    private String login;
+    private String senha;
+    private String acesso;
+    private String status;
+
+    public Usuario(int id, String login, String senha, String acesso, String status) {
+        this.id = id;
+        this.login = login;
+        this.senha = senha;
+        this.acesso = acesso;
+        this.status = status;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getAcesso() {
+        return acesso;
+    }
+
+    public void setAcesso(String acesso) {
+        this.acesso = acesso;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    
+    
+}
