ddc030236351ef25053eab50447e50f01c7e81ba
Luiz Fernando <74717128+LuizReis-dev@users.noreply.github.com>
2022-09-24 19:30:31 -0300

    Atualizando a consulta que lista as bonificações recebidas por um catador

--- ReciclaMais/java/src/java/dao/BonificacaoDao.java
+++ ReciclaMais/java/src/java/dao/BonificacaoDao.java
@@ -10,7 +10,7 @@ public class BonificacaoDao {
         int contagem = 0;
         try {
             Connection con = ConnectionDao.getConnection();
-            PreparedStatement ps = (PreparedStatement) con.prepareStatement("SELECT sum(quantidade_referente) AS contagem FROM bonificacao WHERE id_catador = ? AND id_material = ? AND status = 'pago'");
+            PreparedStatement ps = (PreparedStatement) con.prepareStatement("SELECT sum(quantidade_referente) AS contagem FROM bonificacao INNER JOIN materias_em_op ON bonificacao.id_mat_em_op = materias_em_op.id INNER JOIN operacao_comercial on materias_em_op.id_operacao_comercial = operacao_comercial.id WHERE operacao_comercial.id_catador = ? AND materias_em_op.id_material = ? AND status = 'pago';");
             ps.setInt(1, idCatador);
             ps.setInt(2, idMaterial);
             ResultSet rs = ps.executeQuery();
