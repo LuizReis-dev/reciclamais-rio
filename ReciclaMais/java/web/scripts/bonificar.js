78ea57d28a94e979fa0c43e4243203939e700ec8
Luiz Fernando <74717128+LuizReis-dev@users.noreply.github.com>
2022-09-26 11:04:30 -0300

    registrando bonificação no banco

--- ReciclaMais/java/web/scripts/bonificar.js
+++ ReciclaMais/java/web/scripts/bonificar.js
@@ -9,7 +9,7 @@ window.addEventListener('load', async () => {
     const tabelaCatadores = document.querySelector('.tabela-controlar');
 
     catadores.forEach((catador) => {
-        tabelaCatadores.innerHTML += `<tr data-valor="${catador.valor_bonificar}" data-qtdreferente="${catador.quantidade_bonificacoes}" data-idmatop="${catador.id_materiais_em_operacao_comercial}">
+        tabelaCatadores.innerHTML += `<tr data-idcatador="${catador.id_catador}" data-nome="${catador.nome_catador}" data-valor="${catador.valor_bonificar}" data-qtdreferente="${catador.quantidade_bonificacoes}" data-idmatop="${catador.id_materiais_em_operacao_comercial}">
             <td>${catador.nome_catador}</td>
             <td>${catador.nome_material}</td>
             <td>${catador.total_vendido} kg</td>
@@ -19,28 +19,52 @@ window.addEventListener('load', async () => {
     });
 
     let bonificarFormObj = {
-        id_material_em_op : 0,
-        quantidade_referente : 0,
-        valor_recomendado : 0
+        nome: '',
+        id_material_em_op: 0,
+        quantidade_referente: 0,
+        valor_recomendado: 0,
+        valor: 0
     }
 
     let botoesBonificar = document.querySelectorAll('.btn-bonificar');
     botoesBonificar.forEach((botao) => {
-        botao.addEventListener('click', ()=> {
+        botao.addEventListener('click', () => {
             let row = botao.parentNode.parentNode;
             bonificarFormObj.id_material_em_op = row.dataset.idmatop;
             bonificarFormObj.quantidade_referente = row.dataset.qtdreferente;
             bonificarFormObj.valor_recomendado = row.dataset.valor;
+            bonificarFormObj.nome = row.dataset.nome;
             montarBonificarForm();
-            console.log(bonificarFormObj);
+
         })
     })
+    let qtdInput = document.querySelector('#qtd-input');
     let modal = document.querySelector('.modal');
     let montarBonificarForm = () => {
+        document.querySelector('#catador-nome').innerHTML = "Catador: " + bonificarFormObj.nome;
+        qtdInput.placeholder = "Valor sugerido: R$" + bonificarFormObj.valor_recomendado;
         modal.style.display = 'flex';
     }
     let btnFecharModal = document.querySelector('.fechar-modal');
-    btnFecharModal.addEventListener('click', () =>{
+    btnFecharModal.addEventListener('click', () => {
         modal.style.display = 'none';
     })
+    let btnConfirmar = document.querySelector('.btn-confirmar');
+    btnConfirmar.addEventListener('click', () => {
+        if(qtdInput.value == ''){
+            window.alert('Digite um valor');
+        } else {
+            bonificarFormObj.valor = qtdInput.value
+            fetch('http://localhost:8080/JavaWeb/bonificacao', {
+                method: "POST",
+                headers: {
+                    "Content-type": "application/json; charset=UTF-8"
+                },
+                body: JSON.stringify(bonificarFormObj)
+            })
+                .then(res => res.json())
+                .then(console.log)
+                .catch(console.log); 
+        }
+    }) 
 });
